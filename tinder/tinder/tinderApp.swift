4cf114997c75a5aed2cc1ef5df4ea2a78037eb9e
sirius <sirius@siriuss-iMac.local>
2023-05-02 16:25:23 +0300

    start project

---
+++ tinder/tinder/tinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinderApp.swift
+//  tinder
+//
+//  Created by sirius on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct tinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
